fbd34218786c6a004d7fa7358710c1d3299efc79
송현주 <shyunju7@naver.com>
2023-06-28 17:21:43 +0900

    ✨ feat : set the form title and description input

--- src/components/TitleContainer/index.tsx
+++ src/components/TitleContainer/index.tsx
@@ -1,16 +1,47 @@
 import * as React from "react";
 import * as S from "./style";
 
-const TitleContainer = () => {
-  // 입력창에서 focusout될 때, 저장 필요
+export interface FormPreferencesProps {
+  title: string;
+  description: string;
+}
 
+interface TitleContainerProps {
+  formPreferences: FormPreferencesProps;
+  handleOnChange: (name: string, value: string) => void;
+  handleOnBlur: (name: string, value: string) => void;
+}
+const TitleContainer = ({
+  formPreferences,
+  handleOnChange,
+  handleOnBlur,
+}: TitleContainerProps) => {
+  const { title, description } = formPreferences;
   return (
     <S.TitleWrapper>
-      <S.Input className="input_title" type="text" placeholder="설문지 제목" />
+      <S.Input
+        className="input_title"
+        type="text"
+        placeholder="설문지 제목"
+        value={title}
+        onChange={({
+          target: { value },
+        }: React.ChangeEvent<HTMLInputElement>) =>
+          handleOnChange("title", value)
+        }
+        onBlur={() => handleOnBlur("title", title)}
+      />
       <S.Input
         className="input_description"
         type="text"
         placeholder="설문지 설명"
+        value={description}
+        onChange={({
+          target: { value },
+        }: React.ChangeEvent<HTMLInputElement>) =>
+          handleOnChange("description", value)
+        }
+        onBlur={() => handleOnBlur("description", description)}
       />
     </S.TitleWrapper>
   );
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,10 +1,38 @@
 import * as React from "react";
 import * as S from "./style";
 import TitleContainer from "../../components/TitleContainer";
+import { useDispatch } from "react-redux";
+import { updateFormDescription, updateFormTitle } from "../../reducers/form";
 const Main = () => {
+  const dispatch = useDispatch();
+  const [formPreferences, setFormPreferences] = React.useState({
+    title: "제목 없는 설문지",
+    description: "",
+  });
+
+  const handleOnChangeFormPreferences = (name: string, value: string) => {
+    console.log(name, value);
+    setFormPreferences({
+      ...formPreferences,
+      [name]: value,
+    });
+  };
+
+  const handleOnBlur = (name: string, value: string) => {
+    if (name === "title") {
+      dispatch(updateFormTitle(value));
+      return;
+    }
+    dispatch(updateFormDescription(value));
+  };
+
   return (
     <S.MainContainer>
-      <TitleContainer />
+      <TitleContainer
+        formPreferences={formPreferences}
+        handleOnChange={handleOnChangeFormPreferences}
+        handleOnBlur={handleOnBlur}
+      />
     </S.MainContainer>
   );
 };
