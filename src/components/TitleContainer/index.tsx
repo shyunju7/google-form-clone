b9589bc587c8e7730d6d85024b89ebc9d50cb1b2
송현주 <shyunju7@naver.com>
2023-07-04 20:27:27 +0900

    ♻️ refactor : import문 변경

--- src/components/TitleContainer/index.tsx
+++ src/components/TitleContainer/index.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import * as S from "./style";
 import { useLocation } from "react-router-dom";
 import { StyleSheetManager } from "styled-components";
