6bfbb36555d846bf5cfb2a5c968cb6f837ca32f1
송현주 <shyunju7@naver.com>
2023-07-04 21:06:42 +0900

    📄 add : Create NotFound style file & Notfound page settings

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -1,2 +1,8 @@
-const NotFound = () => <div>NOT FOUND</div>;
+import * as S from "./style";
+const NotFound = () => (
+  <S.NotFoundWrapper>
+    <S.CodeText>404</S.CodeText>
+    <S.GuideText>NOT FOUND</S.GuideText>
+  </S.NotFoundWrapper>
+);
 export default NotFound;
---
+++ src/pages/Error/style.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const NotFoundWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.25rem;
+  color: var(--text-color);
+`;
+
+export const CodeText = styled.h1`
+  text-align: center;
+  font-size: 10rem;
+  font-weight: bold;
+  margin-bottom: 0.75rem;
+  color: var(--primary-color);
+`;
+
+export const GuideText = styled.h2`
+  text-align: center;
+  font-size: 2.5rem;
+  font-weight: 500;
+  margin-bottom: 1.75rem;
+`;
